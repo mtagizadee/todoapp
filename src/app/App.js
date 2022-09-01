aff3329a2d3c7b3aaca6dcf411c1b8f0104dca5d
mtagizadee <magatagi0@gmail.com>
2022-09-02 01:54:53 +0400

    tailwindcss was added and project was cleaned

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='flex items-center justify-center w-screen h-screen'>
+          <p> App </p>
+        </div>
+    );
+};
+
+export default App;
